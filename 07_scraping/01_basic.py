b8fd8bdbf81b6902c4233b3c1333b64d7338edd0
lnViztor <vicjso@gmail.com>
2025-08-12 20:24:18 +0200

    Ajustando las identaciones y comentarios de los archivos de scraping

--- 07_scraping/01_basic.py
+++ 07_scraping/01_basic.py
@@ -8,21 +8,21 @@ url = 'https://www.apple.com/es/shop/buy-mac/macbook-air/'
 response = requests.get(url)
 
 if response.status_code == 200:
-  print('La petición fue exitosa')
+    print('La petición fue exitosa')
 
-  html = response.text
-  print(html)
+    html = response.text
+    print(html)
 
-  # regular expression para encontrar el precio
-  price_pattern = r'<span class="rc-prices-fullprice">(.*?)</span>'
-  match = re.search(price_pattern, html)
+    # regular expression para encontrar el precio
+    price_pattern = r'<span class="rc-prices-fullprice">(.*?)</span>'
+    match = re.search(price_pattern, html)
 
-  if match:
-    print(f"El precio del producto es: {match.group(1)}")
+    if match:
+        print(f"El precio del producto es: {match.group(1)}")
 
-  # get the title if the patter is found
-  title_pattern = r'<title>(.*?)</title>'
-  match = re.search(title_pattern, html)
+    # get the title if the patter is found
+    title_pattern = r'<title>(.*?)</title>'
+    match = re.search(title_pattern, html)
 
-  if match:
-    print(f"El título de la web es: {match.group(1)}")
+    if match:
+        print(f"El título de la web es: {match.group(1)}")
